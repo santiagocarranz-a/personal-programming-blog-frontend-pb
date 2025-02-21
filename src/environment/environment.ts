f287046a594dd30857c10a11fe57b0cae1e09ef7
Santiago Carranza <santiago.c.trjo@gmail.com>
2025-02-21 14:47:20 -0300

    Add date to articles in the Home.
    Update the presentation in 'author data'.
    
    Update typographic and background colors.
    
    Add the current date to the footer.
    
    Modify route in environment.

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,4 +1,5 @@
 export const config = { 
-    localHost:'http://localhost:3000',
-    api:''
+    // localHost:'http://localhost:3000',
+    localHost:'http://localhost:4200/',
+    api:'https://santiagocarranza.net.ar/prueba08'
 }
